f8f97a2e2d070994334618d7563845aa36bd5687
Ketanaa <86412087+Ketanaa@users.noreply.github.com>
2021-07-08 18:33:23 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var gardenImg,rabbitImg, appleImg, orangeLeafImg;
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
-  orangeLeafImg = loadImage(orangeleaf.png);
+  orangeLeafImg = loadImage("orangeleaf.png");
 
 }
 
@@ -18,8 +18,8 @@ garden.addImage(gardenImg);
 
 //creating boy running
 rabbit = createSprite(180,340,30,30);
-rabbit.scale =0.09;
 rabbit.addImage(rabbitImg);
+rabbit.scale =0.09;
 }
 
 
@@ -38,22 +38,35 @@ function draw() {
 }
 
 
-function createApples(){
+function createApples()
+{
   // write your code here 
-  if (frameCount%80 === 0)
-  {
   apple=createSprite(random(50,150),40,10,10);
   appleImg = loadImage(apple.png);
   apple.scale=0.4;
-  }
+  apple.velocity.Y= 4; 
 
 }
 
-function createOrangeLeaf(){
+function createOrangeLeaf()
+{
   // write your code here 
-  if (frameCount%80 === 0)
-  {
   orangeLeaf=createSprite(random(50,150),40,10,10);
   orangeLeaf = loadImage(orangeleaf.png);
   orangeLeaf.scale=0.4;
+  orangeLeaf.velocity.Y= 4; 
+}
+
+var select_sprites=math.round(random(1,2));
+
+if(frameCount%80 === 0)
+{
+  if(select_sprites == 1)
+  {
+    createApples()
   }
+  if(select_sprites == 2)
+  {
+    createOrangeLeaf()
+  }
+}
